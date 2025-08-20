c19b0ab4acb5f42373aebc05cac575b1b2874113
julio763284 <julio763284@gmail.com>
2025-08-20 16:17:19 -0500

    Creacion de la vista main

---
+++ app/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black12,
+          title: Text(' Datos Empresariales ', style: TextStyle(color: Colors.white , fontStyle: FontStyle.italic , fontSize: 40),), centerTitle: true,),
+        body: Padding(
+          padding: const EdgeInsets.all(40),
+          child: Column(
+            children: [
+               Text("Empresa 1" ,style: TextStyle(color: Colors.white , fontSize: 20,),),
+               SizedBox(height: 10),
+               Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(),
+              ),
+              )
+
+            ],
+
+          ),
+
+          ),
+          
+      ),
+    );
+    
+  }
+}
+
