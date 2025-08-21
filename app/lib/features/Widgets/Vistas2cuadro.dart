89ed4a1b219dc0099aeb84ea88779f9ef3ab6cca
julio763284 <julio763284@gmail.com>
2025-08-20 21:06:56 -0500

    Creacion de los eventos bloc

---
+++ app/lib/features/Widgets/Vistas2cuadro.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Cuadro2 extends StatelessWidget {
+  const Cuadro2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
